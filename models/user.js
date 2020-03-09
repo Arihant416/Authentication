44f3dbf861bfdafe5be69b9d5a8a264e0b889add
Arihant jain <arihantjain416@gmail.com>
2020-03-09 17:37:45 +0530

    updated user.js

--- models/user.js
+++ models/user.js
@@ -5,7 +5,7 @@ var UserSchema= new mongoose.Schema({
     username:String,
     password:String
 });
-
+//Plugin saves us from writing 10-15 additional lines of codes in our App.js file(Passport Js) saved us.
 UserSchema.plugin(passportLocalMongoose);
 module.exports=mongoose.model("User",UserSchema);
 
